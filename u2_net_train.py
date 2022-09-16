d1a47d8e26f22b8e5486db384f222716fc274606
Frauke Albrecht <albrecht@dkrz.de>
2022-09-16 15:27:07 +0200

    u2-net

--- u2_net_train.py
+++ u2_net_train.py
@@ -20,6 +20,8 @@ from sklearn.model_selection import train_test_split
 import matplotlib.image as mpimg
 from PIL import Image
 
+import u2_net
+
 class FSDataset(Dataset):
 
     def __init__(self, images, masks, transforms=None):
@@ -49,105 +51,6 @@ class FSDataset(Dataset):
 
         return img, mask
 
-class ConvBlock(nn.Module):
-    '''convolutional block'''
-    def __init__(self, in_c, out_c, args):
-        super(ConvBlock, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=args.ks_convblock, stride=args.stride, padding='same')
-        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=args.ks_convblock, stride=args.stride, padding='same')
-        self.relu = nn.ReLU(inplace=True)
-        self.bn = nn.BatchNorm2d(out_c)
-        self.batch_n = args.batch_n
-
-    def forward(self, x):
-
-        x = self.conv1(x)
-        x = self.relu(x)
-        if self.batch_n:
-            x = self.bn(x)
-
-        x = self.conv2(x)
-        x = self.relu(x)
-        if self.batch_n:
-            x = self.bn(x)
-                
-        return x
-
-class EncoderBlock(nn.Module):
-    '''encoder block'''
-    def __init__(self, in_c, out_c, args):
-        super(EncoderBlock, self).__init__()
-        
-        self.conv = ConvBlock(in_c, out_c, args)
-        self.pool = nn.MaxPool2d(args.pool)
-
-    def forward(self, c):
-
-        c = self.conv(c)
-        p = self.pool(c)
-
-        return c, p # return convolutional (c) part for concatenating
-
-class DecoderBlock(nn.Module):
-    '''
-    decoder block
-    skip_features:: result from conv block to concatenate
-    '''
-    def __init__(self, in_c, out_c, args):
-        super(DecoderBlock, self).__init__()
-        
-        #self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.up = nn.ConvTranspose2d(in_c, in_c//2, kernel_size=2, stride=2)#args.stride)
-        self.conv = ConvBlock(in_c, out_c, args)
-
-    def forward(self, x, skip_features):
-   
-        x = self.up(x)
-        c = torch.cat([x, skip_features], dim=1)
-        c = self.conv(c)
-
-        return c
-
-class FSUNet(nn.Module):
-
-    def __init__(self, in_c, out_c, args):
-        super(FSUNet, self).__init__()
-        
-        self.enc1 = EncoderBlock(args.channels, out_c, args)
-
-        self.enc2 = EncoderBlock(out_c, int(2*out_c), args)
-        self.enc3 = EncoderBlock(int(2*out_c), int(2*2*out_c), args)
-        self.enc4 = EncoderBlock(int(2*2*out_c), int(2*2*2*out_c), args)
-
-        self.conv = ConvBlock(int(2*2*2*out_c), int(2*2*2*2*out_c), args)
-
-        self.dec1 = DecoderBlock(int(2*2*2*2*out_c), int(2*2*2*out_c), args)
-        self.dec2 = DecoderBlock(int(2*2*2*out_c), int(2*2*out_c), args)
-        self.dec3 = DecoderBlock(int(2*2*out_c), int(2*out_c), args)
-        self.dec4 = DecoderBlock(int(2*out_c), out_c, args)
-
-        self.output = nn.Conv2d(out_c, args.classes, kernel_size=1)
-
-
-    def forward(self, img):
-        # input shape (bs, channels, height, width)
-        c1, p1 = self.enc1(img)
-        c2, p2 = self.enc2(p1)
-        c3, p3 = self.enc3(p2)
-        c4, p4 = self.enc4(p3)
-
-        b = self.conv(p4)
-        
-        x = self.dec1(b, c4)
-        x = self.dec2(x, c3)
-        x = self.dec3(x, c2)
-        x = self.dec4(x, c1) 
-        
-        x = self.output(x)
-
-        return x
-
 def dice_coef(y_pred, y_true, smooth=1):
 
     intersection = torch.sum(y_true * y_pred, axis=[1,2,3])
@@ -164,7 +67,24 @@ def iou_coef(y_true, y_pred, smooth=1):
 
     return iou
 
-def training(model, epoch, device, dataloader, criterion, optimizer, args):
+bce_loss = nn.BCELoss(size_average=True)
+
+def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
+
+	loss0 = bce_loss(d0,labels_v)
+	loss1 = bce_loss(d1,labels_v)
+	loss2 = bce_loss(d2,labels_v)
+	loss3 = bce_loss(d3,labels_v)
+	loss4 = bce_loss(d4,labels_v)
+	loss5 = bce_loss(d5,labels_v)
+	loss6 = bce_loss(d6,labels_v)
+
+	loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5 + loss6
+	print("l0: %3f, l1: %3f, l2: %3f, l3: %3f, l4: %3f, l5: %3f, l6: %3f\n"%(loss0.data.item(),loss1.data.item(),loss2.data.item(),loss3.data.item(),loss4.data.item(),loss5.data.item(),loss6.data.item()))
+
+	return loss0, loss
+
+def training(model, epoch, device, dataloader, optimizer, args):
     train_losses = []
     predictions = []
     dice_scores = []
@@ -177,12 +97,16 @@ def training(model, epoch, device, dataloader, criterion, optimizer, args):
         
         # set to device
         img, mask = img.to(device), mask.to(device)
+        print('training')
+        print(img.size(), mask.size())
         # set optimizer to zero
         optimizer.zero_grad()
         # forward pass (apply model)
-        pred = model(img)
+        d0, d1, d2, d3, d4, d5, d6 = model(img)
+        pred = d0
         # loss
-        loss = criterion(pred, mask)
+        #loss = criterion(pred[0], mask)
+        loss0, loss = muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, mask)
         train_losses.append(loss)
         # backward
         loss.backward()
@@ -201,7 +125,7 @@ def training(model, epoch, device, dataloader, criterion, optimizer, args):
         loop.set_description(f'Train Epoch {epoch}/{args.epochs}')
         loop.set_postfix(loss=loss.item(), dice=dice.item(), iou=iou.item())
 
-    # train loss over all batche
+    # train loss over all batches
     train_loss = torch.mean(torch.tensor(train_losses))
     train_dice = torch.mean(torch.tensor(dice_scores))
     train_iou = torch.mean(torch.tensor(iou_scores))
@@ -209,7 +133,7 @@ def training(model, epoch, device, dataloader, criterion, optimizer, args):
     
     return train_loss, train_dice, train_iou
 
-def validation(model, epoch, device, dataloader, criterion, args):
+def validation(model, epoch, device, dataloader, args):
     val_losses = []
     predictions = []
     dice_scores = []
@@ -224,11 +148,13 @@ def validation(model, epoch, device, dataloader, criterion, args):
             # set to device
             img, mask = img.to(device), mask.to(device)
             # forward pass (apply model)
-            pred = model(img)
+            d0, d1, d2, d3, d4, d5, d6 = model(img)
+            pred = d0
             # loss
-            loss = criterion(pred, mask)
+            #loss = criterion(pred[0], mask)
+            loss0, loss = muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, mask)
             val_losses.append(loss)
-             
+              
             pred = (pred > args.threshold).float()
             predictions.append(pred)
             
@@ -236,7 +162,6 @@ def validation(model, epoch, device, dataloader, criterion, args):
             dice_scores.append(dice)
             iou = iou_coef(pred, mask)
             iou_scores.append(iou)
-            
 
             loop.set_description(f'Val Epoch {epoch}/{args.epochs}')
             loop.set_postfix(loss=loss.item(), dice=dice.item(), iou=iou.item())
@@ -329,9 +254,7 @@ def main(args):
     print(f'device: {device}')
     
     # define model
-    in_c = args.channels
-    out_c = 64 
-    model = FSUNet(in_c, out_c, args).to(device)
+    model = u2_net.U2Net(args).to(device)
 
     #data, target = next(iter(train_dataloader))
     #print(model(data).shape)
@@ -341,7 +264,7 @@ def main(args):
 
     # define loss
     if args.classes == 1:
-        criterion = nn.BCEWithLogitsLoss()
+        criterion = nn.BCELoss()#(size_average=True)
     else:
         criterion = nn.CrossEntropyLoss()
 
@@ -359,8 +282,8 @@ def main(args):
 
     for epoch in range(args.epochs):
 
-        train_loss, dice_train, iou_train = training(model, epoch, device, train_dataloader, criterion, optimizer, args)
-        val_loss, dice_val, iou_val, preds = validation(model, epoch, device, val_dataloader, criterion, args)
+        train_loss, dice_train, iou_train = training(model, epoch, device, train_dataloader, optimizer, args)
+        val_loss, dice_val, iou_val, preds = validation(model, epoch, device, val_dataloader, args)
         
         # save model if loss is lower than best_loss
         if args.save and (train_loss < best_loss):
@@ -444,18 +367,20 @@ if __name__ == '__main__':
     parser.add_argument('--save', action='store_true', default=False) # if True, best model and predictions are saved o disk
     # data properties
     parser.add_argument('--img-size', type=int, default=64)
-    parser.add_argument('--channels', type=int, default=3)
+    #parser.add_argument('--channels', type=int, default=3)
     parser.add_argument('--classes', type=int, default=1) # 1 for binary classification
     # train parameters
     parser.add_argument('--batch-size', type=int, default=64)
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--pool', type=int, default=2) # pool size
-    parser.add_argument('--ks-convblock', type=int, default=3) # kernel size of conv block
-    parser.add_argument('--stride', type=int, default=1) # stride size of conv block
-    parser.add_argument('--batch_n', action='store_true') # apply batch norm
+    #parser.add_argument('--ks-convblock', type=int, default=3) # kernel size of conv block
+    #parser.add_argument('--stride', type=int, default=1) # stride size of conv block
+    #parser.add_argument('--batch_n', action='store_true') # apply batch norm
     parser.add_argument('--lr', type=float, default=0.001) # learning rate
     parser.add_argument('--threshold', type=float, default=0.5) # learning rate
-    
+    parser.add_argument('--in_ch', type=int, default=3)
+    parser.add_argument('--out_ch', type=int, default=1)
+
     args = parser.parse_args()
 
     print('BEGIN argparse key - value pairs')
